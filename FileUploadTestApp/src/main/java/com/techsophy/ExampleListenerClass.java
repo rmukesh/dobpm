6dfcae9077a8bc6885784ad4c7dfc088cb6babe4
rmukesh <mukesh@techsophy.com>
2017-11-27 16:52:55 +0530

    empty folder structure

---
+++ FileUploadTestApp/src/main/java/com/techsophy/ExampleListenerClass.java
@@ -0,0 +1,13 @@
+package com.techsophy;
+
+public class ExampleListenerClass {
+
+		public void testMethod() {
+			// TODO Auto-generated method stub
+			System.out.println("test");
+			
+		}
+
+	}
+
+
---
+++ FileUploadTestApp/src/test/java/com/testclasses/ExampleClassUnitTest.java
@@ -0,0 +1,13 @@
+package com.testclasses;
+
+import org.junit.Test;
+
+public class ExampleClassUnitTest {
+
+	@Test
+	public void testMethod() {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+}
